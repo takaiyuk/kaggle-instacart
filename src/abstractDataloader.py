260623c3e442671e2205650bf79ac89e9c394ef4
takaiyuk <takaishikawa1225@gmail.com>
2020-01-23 09:06:02 +0900

    Add notebook

--- src/abstractDataloader.py
+++ src/abstractDataloader.py
@@ -5,14 +5,26 @@ import pandas as pd
 class AbstractDataLoader:
     def __init__(self, config):
         self.config = config
-        self.train_path = config["path"]["train"]
-        self.test_path = config["path"]["test"]
-        self.submit_path = config["path"]["sample_submission"]
+        self.train_path = None
+        self.test_path = None
+        self.submit_path = None
+        if "train" in config["path"].keys():
+            self.train_path = config["path"]["train"]
+        if "test" in config["path"].keys():
+            self.test_path = config["path"]["test"]
+        if "sample_submission" in config["path"].keys():
+            self.sample_submission_path = config["path"]["sample_submission"]
 
     def _load(self, path, nrows, parse_dates=None, index_col=None):
         return pd.read_csv(
             path, nrows=nrows, parse_dates=parse_dates, index_col=index_col
         )
+
+    def load_csv(self, path, nrows=None):
+        df = self._load(
+            path, nrows=nrows, parse_dates=self.config["column"]["timestamp"],
+        )
+        return df
 
     def load_train(self, path=None, nrows=None):
         if path is None:
@@ -35,7 +47,9 @@ class AbstractDataLoader:
     def load_sample_submission(self, path=None, nrows=None):
         if path is None:
             return self._load(
-                self.submit_path, nrows=nrows, index_col=self.config["column"]["index"]
+                self.sample_submission_path,
+                nrows=nrows,
+                index_col=self.config["column"]["index"],
             )
 
     def load_joblib(self, path):
